b701380c3e66cb182607e4384c6fc50888a1cb2a
masonhunk <aoronchu@gmail.com>
2021-04-21 16:39:58 +0800

    no ignore

--- src/main/java/com/webank/blockchain/data/stash/db/mapper/BlockTaskPoolMapper.java
+++ src/main/java/com/webank/blockchain/data/stash/db/mapper/BlockTaskPoolMapper.java
@@ -44,7 +44,7 @@ public interface BlockTaskPoolMapper {
             + ") ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4;")
     public void createTable();
 
-    @Insert("insert ignore into block_task_pool(block_height, certainty ,block_timestamp, updatetime, sync_status)\n"
+    @Insert("insert into block_task_pool(block_height, certainty ,block_timestamp, updatetime, sync_status)\n"
             + "values(#{blockHeight}, #{certainty}, #{blockTime}, #{updatetime}, #{syncStatus})")
     int insertIgnoreInto(BlockTaskPool blockTaskPool);
 
