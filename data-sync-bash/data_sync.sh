cc99ec73517eb5456bd1d21d2b0d1b2392ea2ac2
卡拉马佐夫 <973459080@qq.com>
2021-05-12 11:34:29 +0800

    data-sync

--- data-sync-bash/data_sync.sh
+++ data-sync-bash/data_sync.sh
@@ -2,30 +2,34 @@
 
 #determine the operating system and pull the corresponding execution file
 if [ "$(uname)" == "Darwin" ]; then
-    curl -#LO https://github.com/WeBankBlockchain/Data-Link/releases/download/V1.0.0/data-sync_mac.tar.gz
+    curl -#LO https://github.com/WeBankBlockchain/Data-Stash/releases/download/V1.2.0/data-sync_mac.tar.gz
     tar -zxvf data-sync_mac.tar.gz
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    curl -#LO https://github.com/WeBankBlockchain/Data-Link/releases/download/V1.0.0/data-sync_linux.tar.gz
+    curl -#LO https://github.com/WeBankBlockchain/Data-Stash/releases/download/V1.2.0/data-sync_linux.tar.gz
     tar -zxvf data-sync_linux.tar.gz
 fi
 
-# shellcheck disable=SC2164
-cd data-sync
+current="`pwd`"
 #read the param from config.conf and execute
-CONFIGPATH="../config.conf"
+CONFIGPATH="./config.conf"
 
 STASH_IP=`awk -F '=' '/\[stash\]/{a=1}a==1&&$1~/stash.ip/{print $2;exit}' ${CONFIGPATH}`
 STASH_PORT=`awk -F '=' '/\[stash\]/{a=1}a==1&&$1~/stash.port/{print $2;exit}' ${CONFIGPATH}`
 STASH_DBNAME=`awk -F '=' '/\[stash\]/{a=1}a==1&&$1~/stash.dbname/{print $2;exit}' ${CONFIGPATH}`
 STASH_USERNAME=`awk -F '=' '/\[stash\]/{a=1}a==1&&$1~/stash.username/{print $2;exit}' ${CONFIGPATH}`
 STASH_PASSWORD=`awk -F '=' '/\[stash\]/{a=1}a==1&&$1~/stash.password/{print $2;exit}' ${CONFIGPATH}`
-DATAPATH=`awk -F '=' '/\[data\]/{a=1}a==1&&$1~/sync.dataPath/{print $2;exit}' ${CONFIGPATH}`
 ENDBLOCKNUMBER=`awk -F '=' '/\[more\]/{a=1}a==1&&$1~/sync.endBlockNumber/{print $2;exit}' ${CONFIGPATH}`
 PAGECOUNT=`awk -F '=' '/\[more\]/{a=1}a==1&&$1~/sync.pageCount/{print $2;exit}' ${CONFIGPATH}`
 TABLEPAGECOUNT=`awk -F '=' '/\[more\]/{a=1}a==1&&$1~/sync.bigTablePageCount/{print $2;exit}' ${CONFIGPATH}`
-TYPE=`awk -F '=' '/\[data\]/{a=1}a==1&&$1~/sync.type/{print $2;exit}' ${CONFIGPATH}`
+GROUPID=`awk -F '=' '/\[node\]/{a=1}a==1&&$1~/node.groupId/{print $2;exit}' ${CONFIGPATH}`
+NODEPATH=`awk -F '=' '/\[node\]/{a=1}a==1&&$1~/node.path/{print $2;exit}' ${CONFIGPATH}`
 
-./fisco-sync -n ${ENDBLOCKNUMBER} -i ${STASH_IP} -t ${STASH_PORT} -d ${STASH_DBNAME} -u ${STASH_USERNAME} -p ${STASH_PASSWORD} -c ${DATAPATH} -l ${PAGECOUNT} -s ${TABLEPAGECOUNT} -e ${TYPE}
+# shellcheck disable=SC2164
+cd ${NODEPATH}
+mv ${current}/data-sync/fisco-sync .
 
+./fisco-sync -n ${ENDBLOCKNUMBER} -i ${STASH_IP} -t ${STASH_PORT} -d ${STASH_DBNAME} -u ${STASH_USERNAME} -p ${STASH_PASSWORD} -l ${PAGECOUNT} -s ${TABLEPAGECOUNT} -g ${GROUPID}
+
+cd ${current}
 
 
